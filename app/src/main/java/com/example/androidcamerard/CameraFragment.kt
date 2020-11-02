6f6f40336e71df7e69ebc56367de38ad899081a5
Luke Morris <luke.morris@bondhealth.co.uk>
2020-11-02 14:27:42 +0000

    CameraX added. Image capture now takes place. Viewmodel required to display photo in next fragment.

--- app/src/main/java/com/example/androidcamerard/CameraFragment.kt
+++ app/src/main/java/com/example/androidcamerard/CameraFragment.kt
@@ -1,35 +1,237 @@
 package com.example.androidcamerard
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import android.view.ViewTreeObserver
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
-import com.example.androidcamerard.databinding.FragmentCameraBinding
+import kotlinx.android.synthetic.main.fragment_camera.*
+import kotlinx.android.synthetic.main.fragment_camera.view.*
+import java.io.File
+import java.lang.Exception
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+typealias LumaListener = (luma: Double) -> Unit
 
 class CameraFragment : Fragment() {
 
-    private lateinit var binding: FragmentCameraBinding
+    private var preview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var camera: Camera? = null
+
+    private var outputDirectory: File? = null
+    private lateinit var cameraExecutor: ExecutorService
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(
-            inflater,
-            R.layout.fragment_camera,
-            container,
-            false
-        )
 
-        binding.takePhotoButton.setOnClickListener {
-            findNavController().navigate(R.id.action_cameraFragment_to_cameraOutputFragment)
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_camera, container, false)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
-        return binding.root
+        // Set up the listener for take photo button
+        view.camera_capture_button.setOnClickListener { takePhoto() }
+
+        outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        return view
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            preview = Preview.Builder()
+                .build()
+
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                        Log.d(TAG, "Average luminosity: $luma")
+                    })
+                }
+
+            // Select back camera as a default
+            val cameraSelector =
+                CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                    .build()
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                camera = cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture, imageAnalyzer)
+                preview?.setSurfaceProvider(cameraPreview.surfaceProvider)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(context))
+    }
+
+    private fun setupAutoFocus() {
+        cameraPreview.afterMeasured {
+            val factory: MeteringPointFactory = SurfaceOrientedMeteringPointFactory(
+                cameraPreview.width.toFloat(), cameraPreview.height.toFloat()
+            )
+            val centerWidth = cameraPreview.width.toFloat() / 2
+            val centerHeight = cameraPreview.height.toFloat() / 2
+            //create a point on the center of the view
+            val autoFocusPoint = factory.createPoint(centerWidth, centerHeight)
+            try {
+                camera?.cameraControl?.startFocusAndMetering(
+                    FocusMeteringAction.Builder(
+                        autoFocusPoint,
+                        FocusMeteringAction.FLAG_AF
+                    ).apply {
+                        //auto-focus every 1 seconds
+                        setAutoCancelDuration(1, TimeUnit.SECONDS)
+                    }.build()
+                )
+            } catch (e: CameraInfoUnavailableException) {
+            }
+        }
+    }
+
+    private inline fun View.afterMeasured(crossinline block: () -> Unit) {
+        viewTreeObserver.addOnGlobalLayoutListener(object :
+            ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                if (measuredWidth > 0 && measuredHeight > 0) {
+                    viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    block()
+                }
+            }
+        })
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create a time-stamped output file to hold the image
+        val photoFile = File(outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.ENGLISH)
+            .format(System.currentTimeMillis()) + ".jpg")
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(context),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exception.message}", exception)
+                }
+
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    findNavController().navigate(R.id.action_cameraFragment_to_cameraOutputFragment)
+                }
+            }
+        )
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+            activity?.baseContext?.let { it1 ->
+                ContextCompat.checkSelfPermission(
+                    it1, it
+                )
+            } == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getOutputDirectory(): File? {
+        val mediaDir = context?.externalMediaDirs?.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+
+        val defaultFilesDir: File? = activity?.filesDir
+
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else defaultFilesDir
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(activity,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
+
+        private fun ByteBuffer.toByteArray() : ByteArray {
+            rewind() // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data) // Copy the buffer into a byte array
+            return data //Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy) {
+            val buffer = image.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+
+            listener(luma)
+
+            image.close()
+        }
     }
 }
